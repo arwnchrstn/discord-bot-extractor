f7371dbb7055c607cb29fc1a7ed144ace539b744
Arwen Christian Ceres <ceres703@gmail.com>
2025-02-16 19:35:31 +0800

    add port

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import registerEvents from "./utils/registerEvents"
 import MessageQueue from "./class/ConnectMQ"
 import 'dotenv/config'
 import DeadLetterMQ from "./class/DeadLetterMQ"
+import Express, { Request, Response } from 'express'
+
+const app = Express()
 
 const client: any = new Client({
   intents: [
@@ -24,3 +27,12 @@ const deadQueue = DeadLetterMQ.getInstance()
 
 deadQueue.init()
 messageQueue.init()
+
+app.listen(10000, () => {
+  console.log('Listening on port 10000')
+})
+
+app.get('/health', (req: Request, res: Response) => {
+  console.log('Health: ' + new Date().toISOString())
+  res.send('Health')
+})
